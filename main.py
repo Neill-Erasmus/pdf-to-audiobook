1c5b4f7fd38359678f3b3684dd58d531129d9223
Neill Erasmus <neill.erasmus20@gmail.com>
2023-11-21 21:03:54 +0200

    Added documentation.

--- main.py
+++ main.py
@@ -5,6 +5,19 @@ import openai, sys
 CLIENT = openai.OpenAI(api_key="OPENAI_API_KEY")
 
 def GenerateStory(client : openai.OpenAI) -> str:
+    """
+    Generates a short story using OpenAI's GPT-3.5 Turbo model.
+
+    Parameters:
+        client (openai.OpenAI): An instance of the openai.OpenAI class with the API key set.
+
+    Returns:
+        str: A string representing the generated short story.
+
+    Raises:
+        Exception: If an exception occurs during the API call, the function prints an error message and exits the application.
+    """
+    
     try:
         response            = client.chat.completions.create(
             model           = "gpt-3.5-turbo-1106",
@@ -20,6 +33,21 @@ def GenerateStory(client : openai.OpenAI) -> str:
         sys.exit(0)
 
 def GenerateAudio(client : openai.OpenAI, filename : str) -> None:
+    """
+    Generates an audio file (MP3) by converting the text content of a given PDF file using OpenAI's TTS-1 model.
+
+    Parameters:
+        client (openai.OpenAI): An instance of the openai.OpenAI class with the API key set.
+        filename (str): The name of the input PDF file (including extension) located in the root directory.
+
+    Returns:
+        None
+
+    Raises:
+        FileNotFoundError: If the input PDF file is not found, the function prints an error message and exits the application.
+        Exception: If an exception occurs during the API call or file handling, the function prints an error message and exits the application.
+    """
+    
     text : str = ""
     for page in PdfReader(filename).pages:
         text += page.extract_text()
@@ -37,12 +65,32 @@ def GenerateAudio(client : openai.OpenAI, filename : str) -> None:
     sys.exit(0)
 
 def CreatePDF() -> None:
+    """
+    Generates a PDF file containing a short story and saves it in the output/ directory.
+
+    Parameters:
+        None
+
+    Returns:
+        None
+    """
+    
     pdf = canvas.Canvas(filename="output/output.pdf")
     pdf.drawString(10, 10, text=GenerateStory(client=CLIENT))
     pdf.save()
     print("A pdf containing the short story was saved to output/output.pdf!")
 
 def main() -> None:
+    """
+    The main function that controls the user interaction loop.
+
+    Parameters:
+        None
+
+    Returns:
+        None
+    """
+    
     while True:
         prompt : str = input("Would you like to generate a story? (Y/n): ").lower()
         if prompt == "y":
