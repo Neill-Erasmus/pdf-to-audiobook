6f0df152ad26e49a3c80a496e708f22002827f37
Neill Erasmus <neill.erasmus20@gmail.com>
2023-11-21 20:08:42 +0200

    Created relevant pdf functions.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from reportlab.pdfgen import canvas
 from PyPDF2 import PdfReader
 import openai, sys
 
-CLIENT = openai.OpenAI(api_key="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")
+CLIENT = openai.OpenAI(api_key="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")
 
 def GenerateStory(client : openai.OpenAI) -> str:
     response            = client.chat.completions.create(
@@ -28,10 +28,28 @@ def GenerateAudio(client : openai.OpenAI, filename : str) -> None:
     except Exception as e:
         input(f"An exception {e} was raised. Press any key to exit the application...")
         sys.exit(0)
-    response.stream_to_file("output.mp3")
+    response.stream_to_file("output/output.mp3")
+    sys.exit(0)
+
+def CreatePDF() -> None:
+    pdf = canvas.Canvas(filename="output/output.pdf")
+    pdf.drawString(10, 10, text=GenerateStory(client=CLIENT))
+    pdf.save()
 
 def main() -> None:
-    pass
+    prompt : str = input("Would you like to generate a story?: (Y/n)").lower()
+    if prompt == "y":
+        CreatePDF()
+        GenerateAudio(client=CLIENT, filename="output/output.pdf")
+    elif prompt == "n":
+        while True:
+            file = input("Enter the name of your file and ensure that it is in the root directory (example.pdf): ")
+            try:
+                GenerateAudio(client=CLIENT, filename=file)
+            except FileNotFoundError:
+                print("The file was not found! Ensure that you spelt it correctly and that it is in the correct directory.")
+    else:
+        print("invalid input")
 
 if __name__ == "__main__":
     main()
